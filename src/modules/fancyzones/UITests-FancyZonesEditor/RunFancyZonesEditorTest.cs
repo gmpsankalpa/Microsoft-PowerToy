c3782b79dd90aa9387439ed5cbff2fd880136011
seraphima <zykovas91@gmail.com>
2023-11-20 12:01:53 +0100

    click monitor test

--- src/modules/fancyzones/UITests-FancyZonesEditor/RunFancyZonesEditorTest.cs
+++ src/modules/fancyzones/UITests-FancyZonesEditor/RunFancyZonesEditorTest.cs
@@ -3,15 +3,19 @@
 // See the LICENSE file in the project root for more information.
 
 using System.Collections.Generic;
+using FancyZonesEditorCommon.Data;
+using Microsoft.FancyZonesEditor.UITests.Utils;
 using Microsoft.FancyZonesEditor.UnitTests.Utils;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using static FancyZonesEditorCommon.Data.EditorParameters;
 
-namespace UITests_FancyZonesEditor
+namespace Microsoft.FancyZonesEditor.UITests
 {
     [TestClass]
     public class RunFancyZonesEditorTest
     {
         private static FancyZonesEditorSession? _session;
+        private static IOTestHelper? _ioHelper;
         private static TestContext? _context;
 
         private enum Layouts
@@ -38,11 +42,59 @@ namespace UITests_FancyZonesEditor
         public static void ClassInitialize(TestContext testContext)
         {
             _context = testContext;
+
+            // prepare test editor parameters with 2 monitors before launhing the editor
+            EditorParameters editorParameters = new EditorParameters();
+            _ioHelper = new IOTestHelper(editorParameters.File);
+            ParamsWrapper parameters = new ParamsWrapper
+            {
+                ProcessId = 1,
+                SpanZonesAcrossMonitors = false,
+                Monitors = new List<NativeMonitorDataWrapper>
+                {
+                    new NativeMonitorDataWrapper
+                    {
+                        Monitor = "monitor-1",
+                        MonitorInstanceId = "instance-id-1",
+                        MonitorSerialNumber = "serial-number-1",
+                        MonitorNumber = 1,
+                        VirtualDesktop = "{FF34D993-73F3-4B8C-AA03-73730A01D6A8}",
+                        Dpi = 96,
+                        LeftCoordinate = 0,
+                        TopCoordinate = 0,
+                        WorkAreaHeight = 1040,
+                        WorkAreaWidth = 1920,
+                        MonitorHeight = 1080,
+                        MonitorWidth = 1920,
+                        IsSelected = true,
+                    },
+                    new NativeMonitorDataWrapper
+                    {
+                        Monitor = "monitor-2",
+                        MonitorInstanceId = "instance-id-2",
+                        MonitorSerialNumber = "serial-number-2",
+                        MonitorNumber = 2,
+                        VirtualDesktop = "{FF34D993-73F3-4B8C-AA03-73730A01D6A8}",
+                        Dpi = 96,
+                        LeftCoordinate = 1920,
+                        TopCoordinate = 0,
+                        WorkAreaHeight = 1040,
+                        WorkAreaWidth = 1920,
+                        MonitorHeight = 1080,
+                        MonitorWidth = 1920,
+                        IsSelected = false,
+                    },
+                },
+            };
+            _ioHelper.WriteData(editorParameters.Serialize(parameters));
         }
 
         [ClassCleanup]
         public static void ClassCleanup()
         {
+            _ioHelper?.RestoreData();
+            _ioHelper = null;
+
             _context = null;
         }
 
@@ -83,6 +135,23 @@ namespace UITests_FancyZonesEditor
         public void OpenContextMenu() // verify the context menu is opened
         {
             Assert.IsNotNull(_session?.OpenContextMenu(LayoutNames[Layouts.Columns]));
+        }
+
+        [TestMethod]
+        public void ClickMonitor()
+        {
+            Assert.IsNotNull(_session?.GetMonitorItem(1));
+            Assert.IsNotNull(_session?.GetMonitorItem(2));
+
+            // verify that the monitor 1 is selected initially
+            Assert.IsTrue(_session?.GetMonitorItem(1)?.Selected);
+            Assert.IsFalse(_session?.GetMonitorItem(2)?.Selected);
+
+            _session?.Click_Monitor(2);
+
+            // verify that the monitor 2 is selected after click
+            Assert.IsFalse(_session?.GetMonitorItem(1)?.Selected);
+            Assert.IsTrue(_session?.GetMonitorItem(2)?.Selected);
         }
     }
 }
